04fc9df82434afe1ee1edd0770427ae4fd98e494
Matteo <Matteo@PRO-PC999>
2015-09-21 12:24:13 +0200

    Compressione messaggi

--- public_html/server.js
+++ public_html/server.js
@@ -9,6 +9,7 @@ var Node = Node || {};
 
 // Import global modules
 Node.io = require("socket.io-client");
+Node.zlib = require("zlib");
 
 /**
  * @class Definition of Client object
@@ -45,49 +46,72 @@ Node.Server.prototype.connect = function ()
   //
   this.socket.on("connect", function () {
     pthis.parent.log("INFO", "Connected to " + pthis.serverUrl);
+    //
+    // Send list of databases supported by this connector
+    var msg = {};
+    msg.type = Node.Server.messageTypes.init;
+    msg.data = {};
+    msg.data.name = pthis.parent.name;
+    msg.data.dmlist = [];
+    for (var i = 0; i < pthis.parent.datamodels.length; i++) {
+      var dm = {};
+      dm.name = pthis.parent.datamodels[i].name;
+      dm.class = pthis.parent.datamodels[i].class;
+      msg.data.dmlist.push(dm);
+    }
+    //
+    // Send username if is a IDE server
+    if (pthis.ideUserName)
+      msg.userName = pthis.ideUserName;
+    //
+    pthis.sendMessage(msg);
   });
   //
   this.socket.on("cloudServerMsg", function (data) {
-    pthis.parent.log("INFO", "Server onMessage: " + JSON.stringify(data));
-    //
-    // Compose the message of response
-    var msg = {};
-    msg.type = Node.Server.messageTypes.response;
-    msg.sid = data.sid;
-    if (pthis.ideUserName)
-      msg.userName = pthis.ideUserName;
-    if (data.appid)
-      msg.appid = data.appid;
-    if (data.cbid)
-      msg.cbid = data.cbid;
-    msg.data = {};
-    msg.data.name = pthis.parent.name;
-    //
-    var dm = pthis.parent.dataModelByName(data.dm);
-    if (!dm) {
-      pthis.parent.log("ERROR", "datamodel '" + data.dm + "' not found");
+    // Decompress the message
+    Node.zlib.inflate(data, function (error, buffer) {
+      data = JSON.parse(buffer.toString("utf8"));
+      pthis.parent.log("INFO", "Server onMessage: " + JSON.stringify(data));
       //
-      // If command has a callback send response
-      if (data.cbid) {
-        msg.data.error = "datamodel '" + data.dm + "' not found";
-        pthis.sendMessage(msg);
-      }
-    }
-    else {
-      // Ask the datamodel
-      data.server = pthis;
-      dm.onMessage(data, function (result, error) {
+      // Compose the message of response
+      var msg = {};
+      msg.type = Node.Server.messageTypes.response;
+      msg.sid = data.sid;
+      if (pthis.ideUserName)
+        msg.userName = pthis.ideUserName;
+      if (data.appid)
+        msg.appid = data.appid;
+      if (data.cbid)
+        msg.cbid = data.cbid;
+      msg.data = {};
+      msg.data.name = pthis.parent.name;
+      //
+      var dm = pthis.parent.dataModelByName(data.dm);
+      if (!dm) {
+        pthis.parent.log("ERROR", "datamodel '" + data.dm + "' not found");
+        //
         // If command has a callback send response
         if (data.cbid) {
-          if (error)
-            msg.data.error = error.toString();
-          else
-            msg.data.result = result;
-          //
+          msg.data.error = "datamodel '" + data.dm + "' not found";
           pthis.sendMessage(msg);
         }
-      });
-    }
+      }
+      else {
+        // Ask the datamodel
+        data.server = pthis;
+        dm.onMessage(data, function (result, error) {
+          // If command has a callback send response
+          if (data.cbid) {
+            if (error)
+              msg.data.error = error.toString();
+            else
+              msg.data.result = result;
+            //
+            pthis.sendMessage(msg);
+          }
+        });
+      }
+    });
   });
   //
   this.socket.on("connect_error", function (error) {
@@ -109,25 +133,6 @@ Node.Server.prototype.connect = function ()
   this.socket.on("indeError", function (data) {
     pthis.parent.log("INFO", "indeError: " + data.msg);
   });
-  //
-  // Send list of databases supported by this connector
-  var msg = {};
-  msg.type = Node.Server.messageTypes.init;
-  msg.data = {};
-  msg.data.name = this.parent.name;
-  msg.data.dmlist = [];
-  for (var i = 0; i < this.parent.datamodels.length; i++) {
-    var dm = {};
-    dm.name = this.parent.datamodels[i].name;
-    dm.class = this.parent.datamodels[i].class;
-    msg.data.dmlist.push(dm);
-  }
-  //
-  // Send username if is a IDE server
-  if (this.ideUserName)
-    msg.userName = this.ideUserName;
-  //
-  this.sendMessage(msg);
 };
 
 
@@ -137,7 +142,11 @@ Node.Server.prototype.connect = function ()
  */
 Node.Server.prototype.sendMessage = function (msg)
 {
-  this.socket.emit("cloudConnector", msg);
+  // Send message with compression
+  var pthis = this;
+  Node.zlib.deflate(JSON.stringify(msg), function (error, buffer) {
+    pthis.socket.emit("cloudConnector", buffer);
+  });
 };
 
 
