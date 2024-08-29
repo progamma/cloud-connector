133a7463bf064926c257a6c83fa55ecf8cfb540a
Matteo Sirri <Matteo Sirri@PRO-PC018>
2024-08-29 15:02:27 +0200

    Issue #9533: gestita chiave privata di criptazione da chiave di registro

--- public_html/utils.js
+++ public_html/utils.js
@@ -7,8 +7,6 @@
 /* global module */
 
 var Node = Node || {};
-
-
 Node.Utils = function ()
 {
 };
@@ -20,7 +18,7 @@ Node.Utils = function ()
  */
 Node.Utils.bufferToStream = function (buffer)
 {
-  let stream = new require('stream').Readable();
+  let stream = new require("stream").Readable();
   stream.push(buffer);
   stream.push(null);
   return stream;
@@ -28,17 +26,24 @@ Node.Utils.bufferToStream = function (buffer)
 
 
 Node.Utils.algorithm = "aes-256-cbc";
-Node.Utils.key = Buffer.from([30, 109, 66, 153, 47, 66, 187, 238, 218, 5, 30, 168, 33, 238, 88, 86, 95, 253, 56, 134, 228, 52, 99, 22, 251, 177, 232, 174, 238, 220, 29, 126]);
-Node.Utils.iv = Buffer.from([157, 22, 24, 132, 59, 136, 219, 139, 49, 175, 15, 214, 90, 102, 202, 4]);
+Node.Utils.key = "1e6d42992f42bbeeda051ea821ee58565ffd3886e4346316fbb1e8aeeedc1d7e";
+Node.Utils.iv = "9d1618843b88db8b31af0fd65a66ca04";
 
 /**
  * Encrypt a text
  * @param {String} text to encrypt
+ * @param {String} key
+ * @param {String} iv
  */
-Node.Utils.encrypt = function (text)
+Node.Utils.encrypt = function (text, key, iv)
 {
+  if (!key)
+    key = Node.Utils.key;
+  if (!iv)
+    iv = Node.Utils.iv;
+  //
   let crypto = require("crypto");
-  let cipher = crypto.createCipheriv(Node.Utils.algorithm, Buffer.from(Node.Utils.key), Node.Utils.iv);
+  let cipher = crypto.createCipheriv(Node.Utils.algorithm, Buffer.from(key, "hex"), Buffer.from(iv, "hex"));
   let encrypted = cipher.update(text);
   encrypted = Buffer.concat([encrypted, cipher.final()]);
   return encrypted.toString("hex");
@@ -48,12 +53,19 @@ Node.Utils.encrypt = function (text)
 /**
  * Decrypt a text
  * @param {String} text to decrypt
+ * @param {String} key
+ * @param {String} iv
  */
-Node.Utils.decrypt = function (text)
+Node.Utils.decrypt = function (text, key, iv)
 {
+  if (!key)
+    key = Node.Utils.key;
+  if (!iv)
+    iv = Node.Utils.iv;
+  //
   let crypto = require("crypto");
   let encryptedText = Buffer.from(text, "hex");
-  let decipher = crypto.createDecipheriv(Node.Utils.algorithm, Buffer.from(Node.Utils.key), Node.Utils.iv);
+  let decipher = crypto.createDecipheriv(Node.Utils.algorithm, Buffer.from(key, "hex"), Buffer.from(iv, "hex"));
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
@@ -78,6 +90,58 @@ Node.Utils.stdTimezoneOffset = function ()
 Node.Utils.isDstObserved = function (d)
 {
   return d.getTimezoneOffset() < Node.Utils.stdTimezoneOffset();
+};
+
+
+/**
+ * Recursively replace environment variables in an object's values
+ * @param {Object} obj
+ */
+Node.Utils.replaceEnvVariables = function (obj)
+{
+  if (!obj)
+    return;
+  //
+  // Iterate over each key in the object
+  for (let [key, value] of Object.entries(obj)) {
+    switch (typeof value) {
+      case "string":
+        // If the value is a string, replace the environment variables
+        // Replace with the environment variable value or the original match if not found
+        obj[key] = value.replace(/%(\w+)%/g, (match, variable) => process.env[variable] || "");
+        break;
+
+      case "object":
+        // If the value is an object, recursively replace its environment variables
+        Node.Utils.replaceEnvVariables(value);
+        break;
+    }
+  }
+};
+
+
+/**
+ * Decrypt/encrypt the passwords of datamodels
+ * @param {Object} config
+ * @param {String} key
+ * @param {Boolean} encrypt
+ */
+Node.Utils.processPasswords = function (config, key, encrypt)
+{
+  config.datamodels?.forEach(dm => {
+    let password = dm.connectionOptions.password;
+    try {
+      dm.connectionOptions.password = Node.Utils.decrypt(password, key, dm.iv);
+    }
+    catch (e) {
+    }
+    //
+    if (encrypt) {
+      // Generate a new iv
+      dm.iv = require("crypto").randomBytes(16).toString("hex");
+      dm.connectionOptions.password = Node.Utils.encrypt(dm.connectionOptions.password, key, dm.iv);
+    }
+  });
 };
 
 
